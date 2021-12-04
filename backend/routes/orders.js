9ef31f29543d7616d4b1ca750dfb8e18b43b2348
hvbairagi <hvbairagi@gmail.com>
2021-12-04 22:58:29 +0530

    EOD

--- backend/routes/orders.js
+++ backend/routes/orders.js
@@ -1,14 +1,129 @@
-const {Order} = require('../models/order');
-const express = require('express');
+const { Order } = require("../models/order");
+const { OrderItem } = require("../models/order-item");
+const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
 
-router.get(`/`, async (req, res) =>{
-    const orderList = await Order.find();
+router.get(`/`, async (req, res) => {
+  const orderList = await Order.find()
+    .populate("user", "name")
+    .sort({ dateOrdered: -1 });
 
-    if(!orderList) {
-        res.status(500).json({success: false})
-    } 
-    res.send(orderList);
-})
+  if (!orderList) {
+    res.status(500).json({ success: false });
+  }
+  res.send(orderList);
+});
 
-module.exports =router;
+router.get(`/:id`, async (req, res) => {
+  if (!mongoose.isValidObjectId(req.params.id)) {
+    res.status(400).send("Invalid Order Id");
+  }
+
+  const order = await Order.findById(req.params.id)
+    .populate("user", "name")
+    .populate("orderItems")
+    .populate({
+      path: "orderItems",
+      populate: { path: "product", populate: "category" },
+    });
+
+  if (!order) {
+    res.status(500).json({ success: false });
+  }
+  res.send(order);
+});
+
+router.post("/", async (req, res) => {
+  const orderItemIds = Promise.all(
+    req.body.orderItems.map(async (orderItem) => {
+      let newOrderItem = new OrderItem({
+        quantity: orderItem.quantity,
+        product: orderItem.product,
+      });
+
+      newOrderItem = await newOrderItem.save();
+
+      return newOrderItem._id;
+    })
+  );
+
+  const orderItemIdsResolved = await orderItemIds;
+
+  const totalPrices = await Promise.all(
+    orderItemIdsResolved.map(async (orderItemId) => {
+      const orderItem = await OrderItem.findById(orderItemId).populate(
+        "product",
+        "price"
+      );
+
+      const totalPrice = orderItem.product.price * orderItem.quantity;
+      return totalPrice;
+    })
+  );
+
+  const totalPrice = totalPrices.reduce((a, b) => a + b, 0);
+
+  let order = new Order({
+    orderItems: orderItemIdsResolved,
+    shippingAddress1: req.body.shippingAddress1,
+    shippingAddress2: req.body.shippingAddress2,
+    city: req.body.city,
+    zip: req.body.zip,
+    country: req.body.country,
+    phone: req.body.phone,
+    status: req.body.status,
+    totalPrice: totalPrice,
+    user: req.body.user,
+  });
+
+  order = await order.save();
+
+  if (!order) return res.status(404).send("The order cannot be created!");
+
+  res.send(order);
+});
+
+router.put("/:id", async (req, res) => {
+  if (!mongoose.isValidObjectId(req.params.id)) {
+    res.status(400).send("Invalid Order Id");
+  }
+
+  const order = await Order.findByIdAndUpdate(
+    req.params.id,
+    {
+      status: req.body.status,
+    },
+    { new: true }
+  );
+
+  if (!order) return res.status(404).send("The order cannot be updated!");
+
+  res.send(order);
+});
+
+router.delete("/:id", (req, res) => {
+  if (!mongoose.isValidObjectId(req.params.id)) {
+    res.status(400).send("Invalid Order Id");
+  }
+
+  Order.findByIdAndRemove(req.params.id)
+    .then(async (order) => {
+      if (order) {
+        await order.orderItems.map(async (orderItem) => {
+          await OrderItem.findByIdAndRemove(orderItem);
+        });
+
+        return res
+          .status(200)
+          .json({ success: true, message: "Order is deleted!" });
+      } else {
+        res.status(404).json({ success: false, message: "Order not found!" });
+      }
+    })
+    .catch((err) => {
+      res.status(400).json({ success: false, error: err });
+    });
+});
+
+module.exports = router;
